2d10813ee44bafa16baf168a458b48d6b2353b16
Ashish Delmiya <ashishdelmiya02@gmail.com>
2026-05-23 01:08:35 +0530

    intitial set up and authentication done using clerk

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <Button>
+        Hello world
+      </Button>
+    </div>
+  )
+}
+
+export default page
